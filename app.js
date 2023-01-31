75aa1ff81f5f6c04d389ee4f5baecdddb10fc4e8
pgtognoni <pgtognoni@gmail.com>
2023-01-31 14:55:25 +0000

    lab practice with layout and partials

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const { response } = require('express');
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 const port = 3000;
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 app.use(express.static('public'));
 
-app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/home.html'));
+app.get('/', (req, res, next) => res.render('home'));
 
-app.get('/about', (req, res, next) => res.sendFile(__dirname + '/views/about.html'))
+app.get('/about', (req, res, next) => res.render('about'))
 
-app.get('/work', (req, res, next) => res.sendFile(__dirname + '/views/work.html'))
+app.get('/work', (req, res, next) => res.render('work'))
 
 app.listen(port, () => console.log('App at port 3000'));
